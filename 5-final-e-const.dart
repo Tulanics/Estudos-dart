3d977410a26651fd6d2ee1acf09a823ae582e5a7
Tulani Conceição da Silva <tulanics@gmail.com>
2025-03-03 19:03:58 -0300

    anotações

--- 5-final-e-const.dart
+++ 5-final-e-const.dart
@@ -2,7 +2,10 @@ void main() {
 
   final canal2 = DateTime.now();
   const String apiKey = 'fnsajkdlnfskjfnaçjksfnksnfdsjknfdsjkf';
-
+/*
+const só aceita valores pre definidos ou seja que não sejam dinamicos, 
+neste caso o DateTime.now() é um valor dinâmico
+*/
   print(canal2);
   print(apiKey);
 
--- 6-nullable.dart
+++ 6-nullable.dart
@@ -3,12 +3,13 @@ void main() {
 
   //nome = "Tulani";
 
-  // if(nome?.length > 0) {
+  // if(nome.length > 0) {
   //   print(nome);
   // }else{
   //   print("Não tem nome");
   // }
-  print(nome?.length ?? "preencha seu nome");
+  print(nome?.length ?? "preencha seu nome"); // cuidado no uso do nullable ele precisa ter um objetivo claro
+  //só a versão 2 em diante do flutter aceita esse comando
   int? preco = null;
   double? promocao = null;
   bool? isAdulto = null;
