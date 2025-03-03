588040ab3b4c29ed1b8b900dc312b07f6484057f
Tulani Conceição da Silva <tulanics@gmail.com>
2025-03-03 19:06:29 -0300

    anotações

--- 6-nullable.dart
+++ 6-nullable.dart
@@ -2,12 +2,12 @@ void main() {
   String? nome = null;
 
   //nome = "Tulani";
-
-  // if(nome.length > 0) {
+//AQUI OCORRE ERRO PQ A COMPARAÇÃO NÃO É POSSÍVEL COM VARIÁVEL NULA, CUIDADO COM O USO DE NULO
+  // if(nome?.length > 0) { 
   //   print(nome);
   // }else{
   //   print("Não tem nome");
-  // }
+  // } 
   print(nome?.length ?? "preencha seu nome"); // cuidado no uso do nullable ele precisa ter um objetivo claro
   //só a versão 2 em diante do flutter aceita esse comando
   int? preco = null;
